10bc760e8d203d1c19aab5dc9f095338ac26f685
qb1968 <mark.allison75@gmail.com>
2024-06-12 14:15:13 -0400

    build

---
+++ build/precache-manifest.5e00667c620125cab969dd4d293a3907.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c07d7b103d37ce8b13aff78a06dd96",
+    "url": "/project/mykery/react-js/demo/index.html"
+  },
+  {
+    "revision": "151238f053c9a9bdc74a",
+    "url": "/project/mykery/react-js/demo/static/css/2.af2598c0.chunk.css"
+  },
+  {
+    "revision": "1d5db490f2c3c1ff0c6d",
+    "url": "/project/mykery/react-js/demo/static/css/main.ecba8cfc.chunk.css"
+  },
+  {
+    "revision": "151238f053c9a9bdc74a",
+    "url": "/project/mykery/react-js/demo/static/js/2.9e6471f4.chunk.js"
+  },
+  {
+    "revision": "1d5db490f2c3c1ff0c6d",
+    "url": "/project/mykery/react-js/demo/static/js/main.6a19d91d.chunk.js"
+  },
+  {
+    "revision": "093f8457717cde477816",
+    "url": "/project/mykery/react-js/demo/static/js/runtime-main.4047d9b7.js"
+  },
+  {
+    "revision": "6a1523ec12ff4b6a7e16d8c50add51c8",
+    "url": "/project/mykery/react-js/demo/static/media/Cardo-Bold.6a1523ec.ttf"
+  },
+  {
+    "revision": "836114552efbc7fabf1b9165a6709d8f",
+    "url": "/project/mykery/react-js/demo/static/media/Cardo-Bold.83611455.woff2"
+  },
+  {
+    "revision": "8cd7c28205ec69990dad045e1fb9ed1d",
+    "url": "/project/mykery/react-js/demo/static/media/Cardo-Bold.8cd7c282.eot"
+  },
+  {
+    "revision": "d2197cd8ef232e3d36f612b64a5546a1",
+    "url": "/project/mykery/react-js/demo/static/media/Cardo-Bold.d2197cd8.woff"
+  },
+  {
+    "revision": "d99cc1bc0e135ba467337adb859e2808",
+    "url": "/project/mykery/react-js/demo/static/media/Cardo-Bold.d99cc1bc.svg"
+  },
+  {
+    "revision": "1277762fe7162edaeb72d9a4a056d9ad",
+    "url": "/project/mykery/react-js/demo/static/media/Cardo-Regular.1277762f.svg"
+  },
+  {
+    "revision": "22d924e0418a0c1296b8f5e9476b56e6",
+    "url": "/project/mykery/react-js/demo/static/media/Cardo-Regular.22d924e0.woff"
+  },
+  {
+    "revision": "36b134dd01c427335473d239f7dd121a",
+    "url": "/project/mykery/react-js/demo/static/media/Cardo-Regular.36b134dd.ttf"
+  },
+  {
+    "revision": "7245295c3fdf8b781239677df6811096",
+    "url": "/project/mykery/react-js/demo/static/media/Cardo-Regular.7245295c.woff2"
+  },
+  {
+    "revision": "d88cc3a6ac2fa04fc89e49f91f4a1cea",
+    "url": "/project/mykery/react-js/demo/static/media/Cardo-Regular.d88cc3a6.eot"
+  },
+  {
+    "revision": "1497b71150d1f485d349f515be05a59d",
+    "url": "/project/mykery/react-js/demo/static/media/DMSans-Bold.1497b711.eot"
+  },
+  {
+    "revision": "5d9b171625b99910f9f31300d52613b0",
+    "url": "/project/mykery/react-js/demo/static/media/DMSans-Bold.5d9b1716.woff"
+  },
+  {
+    "revision": "758620eebb72d60a8a5142c6d835187e",
+    "url": "/project/mykery/react-js/demo/static/media/DMSans-Bold.758620ee.svg"
+  },
+  {
+    "revision": "a2c010b5e64ede17e6fd46820c54611b",
+    "url": "/project/mykery/react-js/demo/static/media/DMSans-Bold.a2c010b5.ttf"
+  },
+  {
+    "revision": "d2286fb42c24c4870191c31cf601b017",
+    "url": "/project/mykery/react-js/demo/static/media/DMSans-Bold.d2286fb4.woff2"
+  },
+  {
+    "revision": "5246c7eb4005a0131e72710b5c0957bc",
+    "url": "/project/mykery/react-js/demo/static/media/DMSans-Regular.5246c7eb.eot"
+  },
+  {
+    "revision": "8feb27f3a766cc95e73725316445e20b",
+    "url": "/project/mykery/react-js/demo/static/media/DMSans-Regular.8feb27f3.woff"
+  },
+  {
+    "revision": "aa01324f1eb1fecac2db3d2e030f5379",
+    "url": "/project/mykery/react-js/demo/static/media/DMSans-Regular.aa01324f.woff2"
+  },
+  {
+    "revision": "c0cc8ad9340417fe157b67b17d766585",
+    "url": "/project/mykery/react-js/demo/static/media/DMSans-Regular.c0cc8ad9.svg"
+  },
+  {
+    "revision": "ee47ebaf86c6ceb3eeee1865858e9534",
+    "url": "/project/mykery/react-js/demo/static/media/DMSans-Regular.ee47ebaf.ttf"
+  },
+  {
+    "revision": "acf80d4c8542551f2227951d61379596",
+    "url": "/project/mykery/react-js/demo/static/media/bio.acf80d4c.JPG"
+  },
+  {
+    "revision": "cc3adfdb2afc9b366203c807a481e7d8",
+    "url": "/project/mykery/react-js/demo/static/media/blonding.cc3adfdb.jpg"
+  },
+  {
+    "revision": "91d7b14d218e0a1d88b3549adfbfeaff",
+    "url": "/project/mykery/react-js/demo/static/media/extensions.91d7b14d.jpg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/project/mykery/react-js/demo/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "32bd2b1c7bc2d007bc7663c2dbb6bf13",
+    "url": "/project/mykery/react-js/demo/static/media/fa-brands-400.32bd2b1c.svg"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/project/mykery/react-js/demo/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/project/mykery/react-js/demo/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/project/mykery/react-js/demo/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/project/mykery/react-js/demo/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "27e9ca4e025868a12b6681b3a2d49008",
+    "url": "/project/mykery/react-js/demo/static/media/fa-regular-400.27e9ca4e.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/project/mykery/react-js/demo/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/project/mykery/react-js/demo/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/project/mykery/react-js/demo/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/project/mykery/react-js/demo/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/project/mykery/react-js/demo/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/project/mykery/react-js/demo/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/project/mykery/react-js/demo/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "e7578a9d4a7fd8a98b9a8683cf722f8b",
+    "url": "/project/mykery/react-js/demo/static/media/fa-solid-900.e7578a9d.svg"
+  },
+  {
+    "revision": "fdb6a7e3a06d6c98ea2702a71c47fc82",
+    "url": "/project/mykery/react-js/demo/static/media/icon-arrow-down.fdb6a7e3.svg"
+  },
+  {
+    "revision": "4d13f1966a0bf1983484580193b5816d",
+    "url": "/project/mykery/react-js/demo/static/media/loader.4d13f196.svg"
+  },
+  {
+    "revision": "a4367dfa5a97c8930390b80a56b491d6",
+    "url": "/project/mykery/react-js/demo/static/media/logo2.a4367dfa.jpeg"
+  },
+  {
+    "revision": "7c14e997ab83db28731740e8450af28a",
+    "url": "/project/mykery/react-js/demo/static/media/others.7c14e997.jpg"
+  }
+]);
